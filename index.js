13f4298f6cd9a433a5008b89df25aeeddf46e5ed
kinppink <648270560@qq.com>
2021-08-30 17:26:23 +0800

    修改画笔颜色

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-let canvas = document.getElementById("canvas");
-let clearBtn = document.querySelector("#clearBtn");
+const canvas = document.getElementById("canvas");
+const clearBtn = document.querySelector("#clearBtn");
+const colorList = document.querySelector("#colorList");
+const colorArray = [
+  "#f52443",
+  "#ffa502",
+  "#f9e459",
+  "#98bc67",
+  "#79d2d2",
+  "#91a7ff",
+  "#9fa0d7",
+  "#ff9393",
+  "#f5eeeb",
+  "#bdc3c7",
+  "#95a5a6",
+  "#7f8c8d",
+  "#34495e",
+];
 canvas.height = document.documentElement.clientHeight;
 canvas.width = document.documentElement.clientWidth;
-
+for (let i = 0; i < colorArray.length; i++) {
+  const li = document.createElement("li");
+  if (i === 0) li.classList.add("selected");
+  li.style.background = colorArray[i];
+  colorList.appendChild(li);
+}
 let isMobile = "ontouchstart" in document.documentElement;
 
 let ctx = canvas.getContext("2d");
-//ctx.strokeStyle = "orange";
 ctx.lineWidth = 4;
 ctx.lineCap = "round";
+ctx.strokeStyle = "#1abc9c";
 let isDrawing = false;
 let last;
 
@@ -42,6 +63,14 @@ if (isMobile) {
 // 清空画布
 clearBtn.addEventListener("click", () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+// 改变画笔颜色
+colorList.addEventListener("click", (e) => {
+  const prev = document.querySelector("#colorList>li.selected");
+  prev.classList.remove("selected");
+  const selected = e.target;
+  selected.classList.add("selected");
+  ctx.strokeStyle = selected.style.background;
 });
 
 function drawLine(x1, y1, x2, y2) {
